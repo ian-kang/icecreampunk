a53926bb4a3d007e0c4052d7b9fc60cf558b74fc
Seongchan Iang Kang <seongchank@gmail.com>
2021-09-16 08:08:03 -0700

    add responsive design

--- src/App.js
+++ src/App.js
@@ -17,17 +17,18 @@ function App({ twitter }) {
   });
   console.log(users);
   return (
-    <>
+    <div className="app">
       <header>
         <h1 className="neon_text">ICECREAMPUNK</h1>
       </header>
+
       <article>Influencers' Tweets created in the last 7 days.</article>
       <div className="container">
         {tweetData.map((data) => (
           <Tweet key={data.id} data={data} users={users} />
         ))}
       </div>
-    </>
+    </div>
   );
 }
 
